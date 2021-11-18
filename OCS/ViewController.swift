37f6939868f1585a18a0111bd4b8f86cbe35531e
Amine <amine.elhajmohamed@gmail.com>
2021-11-18 22:32:59 +0100

    Initial commit

---
+++ OCS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OCS
+//
+//  Created by Mohamed Amine HAJ MOHAMED on 18/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
